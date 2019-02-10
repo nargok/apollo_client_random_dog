0fea13ae5547be0090ad44d974b4c91cdb983945
nargok <nargok@gmail.com>
2019-02-10 12:16:49 +0900

    reload Button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { ApolloProvider, Query } from 'react-apollo';
+import { Button } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 
 import client from './client';
 import { GET_SINGLE_RANDOM_DOG } from './graphql';
 
 class App extends Component {
+
+  refreshPage = () => {
+    window.location.reload();
+  }
+
   render() {
     return (
       <ApolloProvider client={client}>
@@ -20,6 +27,7 @@ class App extends Component {
               return (
                 <div className="App">
                   <h1>ランダム犬画像ギャラリー</h1>
+                  <Button primary onClick={this.refreshPage}>Reload</Button>
                   <div className="dogGallery">
                     <img className="dogImage" src={image} width="300" heigh="200" alt="randomDog" />
                   </div>
